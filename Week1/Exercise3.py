2313b67563981f819dfd147b49f8b24946e53789
ElchiBey <elchibegashvili03@gmail.com>
2025-03-07 16:59:17 +0400

    Week 1, Exercises: 3.

---
+++ Week1/Exercise3.py
@@ -0,0 +1,29 @@
+
+
+class CarManager:
+    def __init__(self):
+        self.cars = []
+
+    def add_car(self, brand, model, year, mileage):
+        self.cars.append({"brand": brand, "model": model, "year": year, "mileage": mileage})
+
+    def drive(self, car, km):
+        car["mileage"] += km
+
+    def get_all_cars(self):
+        return self.cars
+
+
+if __name__ == '__main__': # Car management simulation
+
+    car_manager = CarManager()
+
+    car_manager.add_car("Toyota", "Corolla", 2015, 50000)
+    car_manager.add_car("Honda", "Civic", 2018, 30000)
+    car_manager.add_car("Ford", "Focus", 2020, 20000)
+
+    # Simulate driving
+    car_manager.drive(car_manager.cars[0], 150)
+    car_manager.drive(car_manager.cars[1], 200)
+
+print("Updated Car Data:", car_manager.get_all_cars())
