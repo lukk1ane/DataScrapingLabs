1fd215b086f961a5f1fcc0eb00a94036beb4af34
tls-123321 <117777755+tls-123321@users.noreply.github.com>
2025-04-04 16:48:53 +0400

    Add files via upload

---
+++ task1ABS.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://books.toscrape.com/"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+h3_tags = soup.find_all("h3")
+
+for h3 in h3_tags:
+    title = h3.a["title"]
+    
+    parent = h3.parent
+    
+    price_tag = parent.find("p", class_="price_color")
+    price = price_tag.text if price_tag else "Price not found"
+    
+    print(f"Title: {title}\nPrice: {price}\n")
+
+
+
